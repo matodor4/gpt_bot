e56fc1e4352758e061825f734da0473a5c71a5ef
aleksandr <a.maksimov@bsl.dev>
2023-05-02 16:43:48 +0300

    initial commit

---
+++ src/ts/repository/monga.ts
@@ -0,0 +1,18 @@
+// import { MongoClient } from 'mongodb';
+//
+//
+// const url = 'mongodb://localhost:27017';
+// const dbName = 'mydb';
+//
+//
+// const client = new MongoClient(url);
+//
+//
+//
+// class MongoStorage {
+//     private db:MongoClient
+//     constructor(db:MongoClient) {
+//         this.db = db
+//     }
+//
+// }
