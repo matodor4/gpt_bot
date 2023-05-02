64ab9cc0e5d4cd98ee1e402ebd7fc2c9747f985c
aleksandr <a.maksimov@bsl.dev>
2023-05-02 16:39:58 +0300

    [bot-06] - add handler on text event and dialog context

--- dist/app.js
+++ dist/app.js
@@ -7,19 +7,33 @@ export class Application {
         this.converter = converter;
         this.openAI = openAI;
     }
-    async Voice(href, telegramUserID) {
+    async Voice(ctx, href, telegramUserID) {
         try {
             const oggFilePath = await this.downloader.Fetch(href, telegramUserID);
             const mp3FilePath = await this.converter.ToMp3(oggFilePath, telegramUserID);
             const text = await this.openAI.transcription(mp3FilePath);
             console.log("voice to text", text);
-            const messages = [{ "role": "user", content: text }];
-            return this.openAI.chat(messages);
+            ctx.session.messages.push({ "role": "user", content: text });
+            const resp = await this.openAI.chat(ctx.session.messages);
+            ctx.session.messages.push({
+                role: "assistant",
+                content: resp.content,
+            });
+            ctx.reply(resp.content);
         }
         catch (err) {
             const errMsg = err.message;
             console.log(errMsg);
             throw err;
         }
+    }
+    async Text(ctx, message) {
+        ctx.session.messages.push({ "role": "user", content: message });
+        const resp = await this.openAI.chat(ctx.session.messages);
+        ctx.session.messages.push({
+            role: "assistant",
+            content: resp.content,
+        });
+        ctx.reply(resp.content);
     }
 }
